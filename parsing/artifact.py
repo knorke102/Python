071050d0dc4cc9586c1e68f3337c24d66accc2b1
knroke <97691735+knroke@users.noreply.github.com>
2022-09-22 14:56:45 +0500

    Update artifact.py

--- parsing/artifact.py
+++ parsing/artifact.py
@@ -1,1 +1,17 @@
-
+def request_xpath(xpath, title_element):  # ("//*[@class='navigation-panel']", 'main_module')
+    
+    """Collection"""
+    driver.implicitly_wait(module_time)  # ожидание элемента
+    search = driver.find_element(By.XPATH, xpath)  # поиск элемента, по методу XPATH
+    search.click()  # нажатие
+    time.sleep(screenshot_time)  # таймаут 1 секунда
+    driver.save_screenshot(title_element + '.png')  # скриншот
+    search = driver.page_source  # сбор данных
+    soup = BeautifulSoup(search, 'html.parser')  # обработка парсером
+    get = [text for text in soup.stripped_strings]  # обработка текста
+    with open(artifact_path, "r") as file:  # открытие файла с артефактами
+        for line in file:   # цикл текста с артефактами
+            for el in get:  # цикл обработки текста
+                if line.strip() in el:  # поиск совпадений
+                    with open(title_element + '.txt', 'a') as f:  # открытие файла
+                        f.write(el + '\n')  # запись совпадений
