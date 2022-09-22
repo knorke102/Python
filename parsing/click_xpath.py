5f06450a84829270aaa36f11f70a0fd4fb05f580
knroke <97691735+knroke@users.noreply.github.com>
2022-09-22 14:53:53 +0500

    Update click_xpath.py

--- parsing/click_xpath.py
+++ parsing/click_xpath.py
@@ -1,4 +1,6 @@
 def click_xpath(xpath):  # переход на элемент и нажатие
+
+    """Click"""
     driver.implicitly_wait(module_time)
     search = driver.find_element(By.XPATH, xpath)
     search.click()  # нажатие
