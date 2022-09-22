d3db3c93d40d810656d657f1edd4c85eafa4dc83
knroke <97691735+knroke@users.noreply.github.com>
2022-09-22 14:48:49 +0500

    Delete test.py

--- parsing/test.py
+++
@@ -1,5 +0,0 @@
-    with open("C:/Parsing/artifact.txt", "r") as file:
-        for line in file:
-            if line in get:
-                with open(title_element + '.txt', 'a') as f:
-                    f.write(line + '\n')
